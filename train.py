b3c7d8e07c75359e82e52f9f92b1a8831165a2bd
Brian Ko <briankosw@gmail.com>
2020-12-07 13:04:41 +0000

    Init training and evaluation scripts

---
+++ train.py
@@ -0,0 +1,26 @@
+import hydra
+from omegaconf import DictConfig
+import pytorch_lightning as pl
+
+from data import CIFAR10DataModule
+
+
+@hydra.main(config_name="train")
+def main(cfg: DictConfig):
+    datamodule = CIFAR10DataModule(
+        cfg.dataset_dir,
+        cfg.train_pct,
+        cfg.batch_size,
+        cfg.shuffle,
+        cfg.num_workers,
+        cfg.pin_memory,
+        cfg.seed,
+    )
+    dm.prepare(cfg.stage)
+    dm.setup(cfg.stage)
+    trainer = pl.Trainer()
+    trainer.fit(model, datamodule=datamodule)
+
+
+if __name__ == "__main__":
+    main()
